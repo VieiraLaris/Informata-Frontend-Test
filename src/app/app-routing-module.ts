90205478ede7004302957497b957c9112e903b23
VieiraLaris <lalasarandi@gmail.com>
2025-10-05 23:02:45 -0300

    feat(router): adiciona rota para componentes

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login';
+import { CatalogoComponent } from './catalogo/catalogo';
+import { CadastroComponent } from './cadastro/cadastro';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  
+  { path: 'login', component: LoginComponent },
+  { path: 'catalogo', component: CatalogoComponent },
+  { path: 'cadastro', component: CadastroComponent }, 
+
+  { path: '**', redirectTo: 'login' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

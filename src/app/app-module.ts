474504aed4716cd39ce3016dd334d3974a3b5a3d
VieiraLaris <lalasarandi@gmail.com>
2025-10-05 22:59:59 -0300

    feat(core): configura módulos primeng e componentes

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,22 +1,38 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { CadastroComponent } from './cadastro/cadastro';
+import { LoginComponent } from './login/login';
+import { CatalogoComponent } from './catalogo/catalogo';
 
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+
+import Aura from '@primeuix/themes/aura';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    CadastroComponent,
+    LoginComponent,
+    CatalogoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+
+    CardModule,      
+    InputTextModule, 
+    PasswordModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),

d2f34ad7b57c6c3637feea2ffba85a43418b24a7
VieiraLaris <lalasarandi@gmail.com>
2025-10-07 14:18:01 -0300

    feat(core): implementa lógica de logout/sair e limpeza do LocalStorage

--- src/app/catalogo/catalogo.ts
+++ src/app/catalogo/catalogo.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-catalogo',
   standalone: false,
   templateUrl: './catalogo.html',
-  styleUrl: './catalogo.css'
+  styleUrls: ['./catalogo.css']
 })
 export class CatalogoComponent {
 
+  constructor(private router: Router) {}
+
+  sair() {
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('currentUser');
+    this.router.navigate(['/login']);
+  }
 }
